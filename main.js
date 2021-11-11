24650b6f04add7024f1f76f94c20eedde2daf82e
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-11 10:25:29 -0800

    test_6

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
-const person = {
-    fname:"Ergashev",
-    lname:"Sirojiddin",
-    age:25,
-    key:'12345'
-  };
+// const person = {
+//     fname:"Ergashev",
+//     lname:"Sirojiddin",
+//     age:25,
+//     key:'12345'
+//   };
 
 
-delete person.age;
-  let txt = "";
-  for (let x in person) {
-    txt += person[1] + " ";
+// delete person.age;
+//   let txt = "";
+//   for (let x in person) {
+//     txt += person[1] + " ";
+//   }
+// console.log(txt);
+
+const myObj = {
+    name: "John",
+    age: 30,
+    cars: {
+    car1: "Ford",
+    car2: "BMW",
+    car3: "Fiat"
+    }
   }
-console.log(txt);
+
+  myObj1 = myObj.cars.car2;
+  console.log(myObj1);
