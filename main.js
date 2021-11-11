c252408f7b3dd29805f288731236bdce7893cf3f
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-11 10:32:37 -0800

    test_7

--- main.js
+++ main.js
@@ -13,15 +13,36 @@
 //   }
 // console.log(txt);
 
-const myObj = {
-    name: "John",
-    age: 30,
-    cars: {
-    car1: "Ford",
-    car2: "BMW",
-    car3: "Fiat"
-    }
-  }
+// var dayNumber = getDay();
 
-  myObj1 = myObj.cars.car2;
-  console.log(myObj1);
+// day =
+//   (dayNumber === 0) ? 'Dushanba' :
+//     (dayNumber === 1) ? 'Seshanba' :
+//       (dayNumber === 2) ? 'Chorshanba' :
+//         (dayNumber === 3) ? 'Payshanba' :
+//           (dayNumber === 4) ? 'Chorshanba' :
+//             (dayNumber === 5) ? 'Payshanba' :
+//               (dayNumber === 6) ? 'Juma' : 'hafta kuni tugadi';
+
+// console.log('bugun ' + day.toLowerCase() + '.');
+
+// var
+//   countCandyBoys = 1,
+//   countCandyGirls = 2,
+//   message;
+// switch (countCandyBoys + countCandyGirls) {
+//   case 1:
+//     message = 'Одна конфета';
+//     break;
+//   case 2:
+//   case 3:
+//     message = 'Две или три конфеты';
+//     break;
+//   case 4:
+//     message = 'Четыре конфеты';
+//     break;
+//   default:
+//     message = 'Не одна, не две, не три и не четыре конфеты';
+// }
+
+// console.log(message);
