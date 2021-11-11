e9622b5047d04376a9538732c82e036968d4c1b7
SirojiddinCoder <SirojiddinCoder9998@gmail.com>
2021-11-11 08:32:48 -0800

    test_3

---
+++ js.js
@@ -0,0 +1,47 @@
+
+// for loop
+
+const data = [
+    {
+      name: 'John',
+      friends: [
+        {
+          name: 'Friend',
+        },
+        {
+          name: 'Friend2',
+        },
+        {
+          name: 'Friend3',
+        },
+      ],
+    },
+    {
+      name: 'John2',
+      friends: [
+        {
+          name: 'Friend22',
+        },
+        {
+          name: 'Friend222',
+        },
+        {
+          name: 'Friend322',
+        },
+      ],
+    },
+  ];
+
+  console.log(data[1].friends[0].name, 'friend');
+
+  for (let i = 0; i < data.length; i++) {
+    const friends = data[i].friends;
+
+    for (let j = 0; j < friends.length; j++) {
+      console.log(friends[j].name, 'name');
+    }
+
+    //   console.log(friends, 'friends');
+  }
+
+  console.log('loop beginning');
